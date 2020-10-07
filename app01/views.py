149000b5e31a8e8a804252af2b0fa9f34749d710
zhentaoCoding <zt950607@gmail.com>
2020-10-07 20:14:24 +0800

    register

--- app01/views.py
+++ app01/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, HttpResponse
 from utils.tencent.sms import send_sms_single
 import random
 from django.conf import settings
+from django import forms
+from app01 import models
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 # Create your views here.
 
 
@@ -17,4 +21,27 @@ def send_sms(request):
         return HttpResponse('成功')
     else:
         return HttpResponse(res['errmsg'])
+
+
+class RegisterModelForm(forms.ModelForm):
+    mobile_phone = forms.CharField(label='手机号', validators=[RegexValidator(r'^(1[3|4|5|6|7|8|9])\d{9}$', '手机号格式错误'),])
+    password = forms.CharField(label='密码', widget=forms.PasswordInput())
+    confirm_password = forms.CharField(label='确认密码', widget=forms.PasswordInput())
+    code = forms.CharField(label='验证码', widget=forms.TextInput())
+
+    class Meta:
+        model = models.UserInfo
+        fields = ['username', 'email', 'password', 'confirm_password', 'mobile_phone', 'code']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = '请输入{}'.format(field.label)
+
+
+def register(request):
+    form = RegisterModelForm()
+    return render(request, 'register.html', {'form': form})
+
 
